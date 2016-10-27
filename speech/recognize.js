4026d7aaea37a29b3b79eb38d3c592bb6a98ad22
Etienne Moussillac <etienne.moussillac@allpartsdigital.com>
2016-10-27 11:36:02 +0200

    init

---
+++ speech/recognize.js
@@ -0,0 +1,76 @@
+var google = require('googleapis');
+var async = require('async');
+var fs = require('fs');
+
+var speech = google.speech('v1beta1').speech;
+
+function getAuthClient (callback) {
+  google.auth.getApplicationDefault(function (err, authClient) {
+    if (err) {
+      return callback(err);
+    }
+    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+      authClient = authClient.createScoped([
+        'https://www.googleapis.com/auth/cloud-platform'
+      ]);
+    }
+
+    return callback(null, authClient);
+  });
+}
+
+function prepareRequest (inputFile, callback) {
+  fs.readFile(inputFile, function (err, audioFile) {
+    if (err) {
+      return callback(err);
+    }
+    console.log('Got audio file!');
+    var encoded = new Buffer(audioFile).toString('base64');
+    var payload = {
+      config: {
+        encoding: 'LINEAR16',
+        sampleRate: 16000
+      },
+      audio: {
+        content: encoded
+      }
+    };
+    return callback(null, payload);
+  });
+}
+
+function main (inputFile, callback) {
+  var requestPayload;
+
+  async.waterfall([
+    function (cb) {
+      prepareRequest(inputFile, cb);
+    },
+    function (payload, cb) {
+      requestPayload = payload;
+      getAuthClient(cb);
+    },
+    function sendRequest (authClient, cb) {
+      console.log('Analyzing speech...');
+      speech.syncrecognize({
+        auth: authClient,
+        resource: requestPayload
+      }, function (err, result) {
+        if (err) {
+          return cb(err);
+        }
+        console.log('result:', JSON.stringify(result, null, 2));
+        cb(null, result);
+      });
+    }
+  ], callback);
+}
+
+if (module === require.main) {
+  if (process.argv.length < 3) {
+    console.log('Usage: node recognize <inputFile>');
+    process.exit();
+  }
+  var inputFile = process.argv[2];
+  main(inputFile, console.log);
+}
